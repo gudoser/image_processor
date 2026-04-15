98caf67776d74e7d89c576ee75cdb293dc267152
Andrey Ovchinnikov <anddmiovchinnikov@edu.hse.ru>
2026-04-15 17:33:16 +0300

    Add gitignore

--- build/CMakeFiles/image_processor.dir/image/Color.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/image_processor.dir/image/Color.cpp.o: \
-  /Users/gudoser/ami-Andrei-Ovchinnikov-Andrey_Ovchinnokov/tasks/image_processor/image/Color.cpp \
-  /Users/gudoser/ami-Andrei-Ovchinnikov-Andrey_Ovchinnokov/tasks/image_processor/image/Color.h
